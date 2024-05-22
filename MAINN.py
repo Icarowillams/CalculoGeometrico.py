deac4b5b06f21e262f05d83e61809900119e9f05
ICARO RAMOS <119986789+Icarowillams@users.noreply.github.com>
2024-05-22 10:33:05 -0300

    Add files via upload

---
+++ MAINN.py
@@ -0,0 +1,10 @@
+from atividadeFormaGeometrica import *
+retangulo = Retangulo(5,10)
+retangulo.calculaArea()
+retangulo.calculaPerimetro()
+print(f"Retangulo - area: {retangulo.area}, Perimetro: {retangulo.perimetro}")
+
+triangulo = Triangulo(5,10)
+triangulo.calculaArea()
+triangulo.calculaPerimetro()
+print(f"Triangulo- area: {triangulo.area}, Perimetro: {triangulo.perimetro}")
